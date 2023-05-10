6d09bbe1232ed8b14bd8a2b4b4417defde211e8b
hosnijr <hosnijr@gmail.com>
2023-05-10 14:58:29 -0300

    Create whatsapp.js

---
+++ whatsapp.js
@@ -0,0 +1,1 @@
+<script>window.rwbp={email:'hosnijr@gmail.com',phone:'+5551993188189',message:'Olá, vamos conversar?',lang:'pt-BR'}</script><script defer async src='https://duz4dqsaqembt.cloudfront.net/client/whats.js'></script>
